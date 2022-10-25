4af4a1a6de13c02f9272bcaf4c55bf371c832de7
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-10-25 17:13:58 +0200

    @Entity + Repository

---
+++ src/main/java/net/balmes/api/entity/Asix.java
@@ -0,0 +1,28 @@
+package net.balmes.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data					// Añade los Getters y Setters
+@AllArgsConstructor		// Añade un Constructor completo
+@NoArgsConstructor		// Añade un Constructor vacio
+@ToString				// Añade un ToString
+
+@Entity
+public class Asix {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	final String group = "ASIX";
+    private String name;
+    private String surname;
+    private String email;
+}
