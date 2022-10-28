7cbf30a07f24331ed3b6774461b97ad08c6f5325
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-10-28 17:07:40 +0200

    Get Resuelto

--- src/main/java/net/balmes/api/controller/CursosController.java
+++ src/main/java/net/balmes/api/controller/CursosController.java
@@ -1,36 +1,58 @@
 package net.balmes.api.controller;
 
-import net.balmes.api.objects.Cursos;
+import net.balmes.api.entity.*;
+import net.balmes.api.repository.CursosRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class CursosController {
-
-    @GetMapping("Cursos")
-    public List<Cursos> getCursos() {
-        List<Cursos> CursosCFGS = new ArrayList<>();
-        CursosCFGS.add(new Cursos("ASIX", "ASIX1",1, "21","diurn"));
-        CursosCFGS.add(new Cursos("ASIX", "ASIX2",2, "23","diurn"));
-        CursosCFGS.add(new Cursos("DAM", "DAM1",1, "24","diurn"));
-        CursosCFGS.add(new Cursos("DAM", "DAM2",2, "25","diurn"));
-        CursosCFGS.add(new Cursos("DAW", "DAW1",1, "26","diurn"));
-        CursosCFGS.add(new Cursos("DAW", "DAW2",2, "27","diurn"));
-        CursosCFGS.add(new Cursos("ASIX", "ASIX1",1, "21","nocturn"));
-        CursosCFGS.add(new Cursos("ASIX", "ASIX2",2, "23","nocturn"));
-        CursosCFGS.add(new Cursos("DAM", "DAM1",1,"24","nocturn"));
-        CursosCFGS.add(new Cursos("DAM", "DAM2",2, "25","nocturn"));
-        CursosCFGS.add(new Cursos("DAW", "DAW1",1,"26","nocturn"));
-        CursosCFGS.add(new Cursos("DAW", "DAW2",2, "27","nocturn"));
-
-        // Horario 1 = diurno
-        // Horario 2 = nocturo
-
-        return CursosCFGS;
+	
+	@Autowired
+    CursosRepository cursosRep;
+    
+    public CursosController(CursosRepository userService) {
+        this.cursosRep = userService;
+    }
+    
+    @GetMapping("/api/cursos/{id}")
+    public Cursos byId(@PathVariable("id") long id) {
+        return cursosRep.findById(id).get();
+    }
+    
+    @GetMapping("/api/cursos")
+	public Iterable<Cursos> getClient() {
+		
+		return cursosRep.findAll();
+	}
+   /*
+    @PostMapping("/api/cursos/")
+    public Cursos create(@RequestBody Cursos cursos) {
+        return cursosRep.create(cursos);
     }
 
+    @PutMapping("/api/cursos/")
+    public Cursos update(@RequestBody Cursos cursos) {
+        return cursosRep.update(cursos);
+    }
+    /*
+    @PatchMapping("/api/cursos/")
+    public Cursos change(@RequestBody Cursos cursos) {
+        return cursosRep.change(cursos);
+    }
 
+    @DeleteMapping("/api/cursos/{id}")
+    public Cursos delete(@PathVariable("id") long id) {
+        return cursosRep.deleteById(id).get();
+    }
+*/
 }
